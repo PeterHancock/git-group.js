83215a836e7d8c9bc5ff39a346296ea412e5fe86
Peter Hancock <peter.hancock@gmail.com>
2015-04-05 16:46:17 +0100

    wip

--- src/cmd/add.js
+++ src/cmd/add.js
@@ -1,17 +1,12 @@
 var fs = require('fs-extra');
 var Git = require('nodegit');
-var tmp = require('temporary');
 var Q = require('q');
-var rmdir = require('rmdir');
 var Url = require('url');
-var _ = require('underscore');
 var fs_safe = require('../util/fs');
 var q_extra = require('../util/q-extra');
-
 var attach = require('../attach');
 
 module.exports = function add(config, name) {
-  var dir = new tmp.Dir();
   var projectDir  = config.cwd;
   var wcDir = config.cwd + '/' + name;
   return Q.nfcall(fs_safe.exists, projectDir + '/.git-group')
@@ -28,7 +23,12 @@ module.exports = function add(config, name) {
   .then(function () {
     return Git.Repository.init(wcDir, 0);
   })
-  .then(function () {
-    return attach(wcDir, config);
+  .then(function (repo) {
+    return attach(wcDir, config).then(function () {
+      return repo;
+    });
+  })
+  .then(function (repo) {
+    // TODO checkout branch
   })
 }
--- src/cmd/init.js
+++ src/cmd/init.js
@@ -13,7 +13,6 @@ module.exports = function init(config, url) {
   var projectName = _(u.path.split('/')).last().replace(/\.git/, '');
   var projectDir = config.cwd + '/' + projectName;
   var tmpDir = new tmp.Dir();
-  console.log(u.pathname, projectName, projectDir, tmpDir.path)
   return Q.nfcall(fs_safe.exists, projectDir)
     .then(function (exists) {
       if (exists)
